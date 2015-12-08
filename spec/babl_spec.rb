5f68e9ce40d4d48ba7ad7917b7fa5503659c8c45
Lars Kluge <l@larskluge.com>
2015-12-08 13:21:38 +0700

    Fix specs

--- spec/babl_spec.rb
+++ spec/babl_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Babl do
   it 'has a version number' do
-    expect(Babl::VERSION).not_to be nil
+    expect(Babl.version).not_to be nil
   end
 
   it "checks a module's response" do
