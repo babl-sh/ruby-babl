c1f00347f6f432461b3c44348c6d250d92293af6
Lars Kluge <l@larskluge.com>
2016-01-20 19:10:11 +0100

    Large message issue, add failing specs to demonstrate what’s wrong with large messages and the current RPC approach

--- spec/babl_spec.rb
+++ spec/babl_spec.rb
@@ -8,4 +8,19 @@ describe Babl do
   it "checks a module's response" do
     expect(Babl.module "string-append", in: "foo", env: {APPENDIX: "bar"}).to eq "foobar"
   end
+
+  it "sends a very long message" do
+    n = 1963
+    expect(Babl.module "string-upcase", in: ("foo" * n)).to eq ("FOO" * n)
+  end
+
+  it "resizes an image" do
+    image_in = File.open(File.expand_path("../fixtures/image-resize-in.jpg", __FILE__), "rb") { |f| f.read }
+    image_out = File.open(File.expand_path("../fixtures/image-resize-out.jpg", __FILE__), "rb") { |f| f.read }
+
+    expect(image_in.size).to eq 36968
+    resized = Babl.module('image-resize', in: image_in, env: {WIDTH: 10, HEIGHT: 10, FORMAT: 'jpg'})
+
+    expect(resized.size).to eq image_out.size
+  end
 end
