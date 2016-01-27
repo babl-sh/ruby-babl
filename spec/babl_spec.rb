8048200ba64a1ded29ca1794c06d13687f207925
Lars Kluge <l@larskluge.com>
2016-01-27 16:46:17 +0100

    Namespace module calls

--- spec/babl_spec.rb
+++ spec/babl_spec.rb
@@ -6,12 +6,12 @@ describe Babl do
   end
 
   it "checks a module's response" do
-    expect(Babl.module "string-append", in: "foo", env: {APPENDIX: "bar"}).to eq "foobar"
+    expect(Babl.module "larskluge/string-append", in: "foo", env: {APPENDIX: "bar"}).to eq "foobar"
   end
 
   it "sends a very long message" do
     n = 1963
-    expect(Babl.module "string-upcase", in: ("foo" * n)).to eq ("FOO" * n)
+    expect(Babl.module "larskluge/string-upcase", in: ("foo" * n)).to eq ("FOO" * n)
   end
 
   it "resizes an image" do
@@ -19,7 +19,7 @@ describe Babl do
     image_out = File.open(File.expand_path("../fixtures/image-resize-out.jpg", __FILE__), "rb") { |f| f.read }
 
     expect(image_in.size).to eq 36968
-    resized = Babl.module('image-resize', in: image_in, env: {WIDTH: 10, HEIGHT: 10, FORMAT: 'jpg'})
+    resized = Babl.module('larskluge/image-resize', in: image_in, env: {WIDTH: 10, HEIGHT: 10, FORMAT: 'jpg'})
 
     expect(resized.size).to eq image_out.size
   end
